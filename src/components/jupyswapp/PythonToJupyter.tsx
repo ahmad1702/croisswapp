ae6dd4aafa8cab48d227a27802eeaf43a1275000
Ahmad <ahmad1702@gmail.com>
2023-05-02 19:15:07 -0500

    #6 - 'Window is undefined' build fix

--- src/components/jupyswapp/PythonToJupyter.tsx
+++ src/components/jupyswapp/PythonToJupyter.tsx
@@ -1,31 +1,24 @@
 "use client";
 
 import {
-  IpynbRenderer,
   type Props as IpynbRendererProps
 } from "react-ipynb-renderer";
 // Jupyter theme
 // import "react-ipynb-renderer/dist/styles/oceans16.css";
 import "@/styles/jupyter-notebook.css";
 import { Notebook } from "@/types/jupyter";
+import dynamic from "next/dynamic";
 
 type NotebookWierd = IpynbRendererProps["ipynb"];
 
+const IpynbRenderer = dynamic(() => import('react-ipynb-renderer').then((m) => m.IpynbRenderer), { ssr: false })
+
 type JupyterNotebookViewerProps = {
-  // pythonCode: string;
   value: Notebook;
-  // onChange: (value: string | null) => void;
 };
 const PythonToJupyter = ({
   value,
-  ...props
 }: JupyterNotebookViewerProps) => {
-  // const notebook: Notebook = useMemo(() => {
-  //   const res = pythonToJupyter(pythonCode) as unknown as Notebook
-  //   onChange(JSON.stringify(res, null, 2))
-  //   return res
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [pythonCode]);
   return (
     <div className="h-full w-full relative rounded-xl overflow-hidden">
       <div className="ipynb-viewer absolute left-0 right-0 h-full w-full overflow-auto">
--- src/components/ui/show-hide.tsx
+++ src/components/ui/show-hide.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { cn } from '@/utils/cn'
 import { Transition } from '@headlessui/react'
 import { forwardRef } from 'react'
